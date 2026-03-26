ed19ae10bc2d9fb4330c8c032c89fd3410ed4ad7
picchat <contato@picchat.io>
2026-03-26 17:24:18 -0300

    first commit

---
+++ src/main/java/com/dariodussin/whatsappautomationbackend/WhatsappAutomationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dariodussin.whatsappautomationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappAutomationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappAutomationBackendApplication.class, args);
+    }
+
+}
